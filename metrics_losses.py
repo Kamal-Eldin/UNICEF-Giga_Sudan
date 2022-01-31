6681b42a6895c2197e54850d150c038230ec5cfc
Kamal-Eldin <k.eldin.kamal@gmail.com>
2022-01-31 09:30:14 +0100

    created losses and metrics functions

---
+++ metrics_losses.py
@@ -0,0 +1,86 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from tensorflow.keras.losses import binary_crossentropy , BinaryCrossentropy
+import keras.backend as k
+eps = k.epsilon()
+
+
+def cast_flat(true, pred):
+    true_f = k.cast(true, 'float32')
+    pred_f = k.cast(pred, 'float32')
+    true = k.flatten(true_f)
+    pred = k.flatten(pred_f)
+
+    return true, pred
+
+def iou(true, pred):
+    eps = k.epsilon()
+    true, pred = cast_flat(true, pred)
+    intersection = k.sum(true * pred)
+    union = (k.sum(true) + k.sum(pred)) - intersection
+    iou = (intersection + eps) / (union + eps)
+
+    return iou
+
+def iou_loss(true, pred):
+    return 1 - iou(true, pred)
+
+def dice(true, pred):
+    eps = k.epsilon()
+    true, pred = cast_flat(true, pred)
+    intersection = k.sum(true * pred)
+    union = (k.sum(true) + k.sum(pred)) 
+    dice = (2 * intersection + eps) / (union + eps)
+
+    return dice
+
+def dice_loss (true, pred):
+    return 1 - dice(true, pred)
+
+def dice_xent(true, pred):
+    return dice_loss(true, pred) + binary_crossentropy(true, pred)
+
+###################
+
+def conf_matrix (true, pred, iou_threshold = 0.5):
+  iou_score = iou(true, pred)
+  TP, FP, FN = 0, 0, 0
+  
+  yt, yp = cast_flat(true, pred)
+
+  if k.sum(true) > 0 and k.sum(pred) > 0:
+    if iou_score >= iou_threshold:
+      TP += 1
+    else:
+      FP += 1
+
+  elif k.sum(true) > 0 and k.sum(pred) <= 0:
+    FN += 1
+
+  elif k.sum(true) <= 0 and k.sum(pred) > 0:
+    FP += 1
+
+  TP, FP, FN = (k.cast(count, 'float32') for count in (TP, FP, FN) )
+  return TP, FP, FN
+
+#####################################################
+
+def precsn(true, pred, iou_threshold = 0.5):
+  eps = k.epsilon()
+  TP, FP, FN = conf_matrix (true, pred, iou_threshold)
+  return (TP + eps ) / (TP + FP + eps)
+
+def recall(true, pred, iou_threshold = 0.5):
+  eps = k.epsilon()
+  TP, FP, FN = conf_matrix (true, pred, iou_threshold)
+  return (TP + eps) / (TP + FN + eps )
+
+def mAP (true, pred):
+  prcsn_scores = []
+  for iou_thresold in np.arange(.5,.95,0.05):
+    prcsn_scores.append(precsn(true, pred, iou_thresold))
+
+  mAP = np.sum(prcsn_scores)/len(prcsn_scores)
+  return mAP
+
